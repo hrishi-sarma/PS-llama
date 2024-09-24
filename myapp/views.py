91374b08a5b2d68c59ce857371b762522a37ea69
hrishi-sarma <sarmahrishi05@gmail.com>
2024-09-24 18:46:21 +0530

    homepage WIP

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')  # This will look for an HTML file called 'home.html'
+    return render(request, 'home.html')  
